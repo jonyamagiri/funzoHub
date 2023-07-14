1007def961254fa13ebad4ecdb8f3c1a1f6240b7
Chybuzlopez <danielorihanna@gmail.com>
2023-07-14 20:15:48 +0100

    user authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a user
@@ -20,21 +21,56 @@ const registerUser = asyncHandler(async(req,res) => {
     //Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log("Hashed password: ", hashedPassword);
-    res.json({message: "Register the user"});
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    console.log(`User created ${user}`);
+    if (user) {
+        res.status(200).json({ _id: user.id, email: user.email});
+    } else {
+        res.status(400);
+        throw new Error("User data not valid");
+    };
 });
 
 //@desc Login user
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async(req,res) => {
-    res.json({message: "Login user"});
-});
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id,
+                },
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiration: "10m" }
+        );
+        res.status(200).json({ accessToken });
+    }else {
+        res.status(401);
+        throw new Error("email or password not valid");
+    }
+}); 
 
 //@desc Current user information
 //@route GET /api/users/current
 //@access private
 const currentUser = asyncHandler(async(req,res) => {
-    res.json({message: "Current user information"});
+    res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const { registerUser,
-     loginUser, 
-     currentUser} = require("../controllers/userController");
+const { registerUser,loginUser, currentUser } = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler")
 
 const router = express.Router();
 
@@ -9,6 +8,6 @@ router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
-router.get("/current", currentUser);
+router.get("/current", validateToken, currentUser);
 
 module.exports = router;
